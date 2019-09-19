81b94b9b5277cdb4426ba2c0f1173b3b5830f27d
juanitovaldez <51437764+juanitovaldez@users.noreply.github.com>
2019-09-19 15:20:57 -0500

    functions work. next is set up the formatting and finish writing the get stats functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,23 +8,38 @@ import csv
 
 #Creates objects to input and out put
 # Sample call:
-# budge_data_csv, summary_out_txt = create_budget_io(budget_data.csv, Resources, analysis, summary.txt )
+# budget_data_csv, summary_out_txt = create_budget_io(budget_data.csv, Resources, analysis, summary.txt )
 def create_budget_io(in_file, in_path, out_file, out_path):
     file_load = os.path.join(str(in_path), str(in_file))
     file_output = os.path.join(str(out_path), str(out_file))
     return file_load, file_output
 
-# The date has format MMM-YYY Delimiter is ','
-# Takes a string from our data set 
-# This is annoying because we have negative values in one column
-# While also using a dash in the date. Introduce a two step split:
-# Sample call:
-# reader
-def split_to_tuple(csv_row, delim_1, delim_2):
-    date, p_l = csv_row.split(f'{delim_1}')
-    month, year = date.split(f'{delim_2}')
-    year, month, p_l =split_row
-    return split_row
+#read_csv file to a list of tuples
+def csv_to_list(csv_file):
+   with open(csv_file) as file:
+        budget_data = [tuple(line) for line in csv.reader(file)]
+        return budget_data 
 
+#read a csv to a dic with headers as key: value names
 
-    
+def csv_to_dic(csv_file):
+    budget_data = {}
+    with open(csv_file) as file:
+        reader = csv.DictReader(file)
+        for dict_row in reader:
+            date = dict_row["Date"]
+            p_l = dict_row["Profit/Losses"]
+            budget_data[date] = p_l
+            #print(dict_row["Date"])            
+    return  budget_data
+
+#Testing and execution framework
+budget_data_csv, summary_out_txt = create_budget_io('budget_data.csv', 'Resources', 'summary.txt', 'analysis')
+
+budget_list = csv_to_list(budget_data_csv)
+budget_dict = csv_to_dic(budget_data_csv)
+
+max_p =budget_dict[f'{max (budget_dict, key=budget_dict.get)}']
+
+#print(budget_list)
+print(max_p)
